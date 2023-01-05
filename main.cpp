a495e39f3c99f079a63ce402bf7d03a1b088ad88
ywzeng <ieywzeng@hotmail.com>
2023-01-05 08:37:40 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include "base_type.h"
 #include "pcap.h"
 
-int main()
+int main(int argc, char** argv)
 {
-    const char8_t *filepath = "./data/test_data_1.pcap";
-    vector<vector<string>> pkt_info_vec;
-    parse_pcap(filepath, pkt_info_vec);
+    if (argc <= 1) {
+        printf("Need a pcap file.\n");
+        return 0;
+    }
+
+    // const char8_t *filepath = "../data/test_ddos_data.pcap";
+    for (int i = 1; i < argc; i++) {
+        const char8_t *filepath = argv[i];
+        vector<vector<string>> pkt_info_vec;
+        parse_pcap(filepath, pkt_info_vec);
+    }
     
     return 0;
 }
