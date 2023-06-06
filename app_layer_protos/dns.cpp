ca7155c885487a4f0d083530927fa97764af2f18
ywzeng <ieywzeng@hotmail.com>
2023-06-06 21:12:07 +0800

    Add files via upload

---
+++ app_layer_protos/dns.cpp
@@ -0,0 +1,232 @@
+#include "dns.h"
+
+/**
+ * Extract domain name from the DNS packet RR section. 
+ * Notice the zip mode. Apart from the entire domain name, certain suffix of the domain can also be represented in zip mode.
+ * 
+ * Note that the domain field is represented as 'len-string' pair.
+ * For example, 'baidu.com' is represented as '05 62 61 69 64 75 03 63 6f 6d 00' in HEX format, 
+ *      where '05' and '03' indicate the length of the following bytes, and '00' is the ending symbol.
+ * In this case, the length of 'baidu.com' is 9, but the byte it occupies is 11 in DNS packet.
+ * 
+ * Return the actual occupied bytes of the domain field.
+*/
+uint16_t get_domain(char8_t* dns_pkt, uint16_t offset, string& domain)
+{
+    uint16_t occupied_bytes = 0;
+    bool is_zip = false;
+
+    // Get the occupied bytes of the first label, and check whether it involves zip mode.
+    uchar8_t label_bytes = (dns_pkt + offset)[0];
+    while (label_bytes) {
+        // Check the zip mode flag.
+        if (label_bytes >= DNS_ZIP_FLAG) {
+            memcpy(&offset, dns_pkt + offset, sizeof(uint16_t));
+            // Modify the byte order.
+            offset = SWAP16(offset);
+            // Extract the embedded offset.
+            offset = offset & DNS_ZIP_MASK;
+
+            label_bytes = (dns_pkt + offset)[0];
+
+            if (!is_zip) {
+                is_zip = true;
+                occupied_bytes += sizeof(uint16_t);
+            }
+        } else {
+            // Copy the domain label.
+            char8_t* label = (char8_t*)malloc(label_bytes + 1);     // Consider the '\0' ending symbol.
+            memset(label, '\0', label_bytes + 1);
+            memcpy(label, dns_pkt + offset + 1, label_bytes);       // Skip the 'label_bytes' byte.
+            domain += string(label);
+
+            free(label);
+            label = nullptr;
+
+            if (!is_zip) {
+                // Consider the 'label_bytes' byte at the beginning of the label field.
+                occupied_bytes += 1+ label_bytes;
+            }
+
+            offset += 1 + label_bytes;
+            label_bytes = (dns_pkt + offset)[0];
+
+            if (label_bytes) {
+                domain += ".";
+            }
+        }
+    }
+    
+    // Consider the '\0' ending symbel at the end of the domain field.
+    if (!is_zip) {
+        occupied_bytes++;
+    }
+
+    return occupied_bytes;
+}
+
+void free_question_section(vector<PcapDNSQuestion>& dns_question_list)
+{
+    for (auto iter = dns_question_list.begin(); iter != dns_question_list.end(); iter++) {
+        if (iter->name != nullptr) {
+            free(iter->name);
+            iter->name = nullptr;
+        }
+    }
+}
+
+void free_rr_section(vector<PcapDNSRR>& dns_rr_list)
+{
+    for (auto iter = dns_rr_list.begin(); iter != dns_rr_list.end(); iter++) {
+        if (iter->name != nullptr) {
+            free(iter->name);
+            iter->name = nullptr;
+        }
+        if (iter->rdata != nullptr) {
+            free(iter->rdata);
+            iter->rdata = nullptr;
+        }
+    }
+}
+
+void get_question_section_info(char8_t* dns_pkt, uint16_t& offset, PcapDNSQuestion& cur_ques_sec)
+{
+    // Get domain.
+    string temp_domain;
+    uint16_t dm_occupied_len = get_domain(dns_pkt, offset, temp_domain);
+    cur_ques_sec.name = (char8_t*)malloc(temp_domain.length() + 1);
+    memset(cur_ques_sec.name, '\0', temp_domain.length() + 1);
+    temp_domain.copy(cur_ques_sec.name, temp_domain.length(), 0);
+
+    // Get domain length.
+    cur_ques_sec.name_len = strlen(cur_ques_sec.name);
+
+    // Get query type.
+    offset += dm_occupied_len;
+    memcpy(&cur_ques_sec.qtype, dns_pkt + offset, sizeof(uint16_t));
+    cur_ques_sec.qtype = SWAP16(cur_ques_sec.qtype);
+
+    // Get query class.
+    offset += sizeof(uint16_t);
+    memcpy(&cur_ques_sec.qclass, dns_pkt + offset, sizeof(uint16_t));
+    cur_ques_sec.qclass = SWAP16(cur_ques_sec.qclass);
+
+    offset += sizeof(uint16_t);
+}
+
+void get_rr_section_info(char8_t* dns_pkt, uint16_t& offset, PcapDNSRR& cur_rr_sec)
+{
+    // Get domain.
+    string temp_domain;
+    uint16_t dm_occupied_len = get_domain(dns_pkt, offset, temp_domain);
+    cur_rr_sec.name = (char8_t*)malloc(temp_domain.length() + 1);
+    memset(cur_rr_sec.name, '\0', temp_domain.length() + 1);
+    temp_domain.copy(cur_rr_sec.name, temp_domain.length(), 0);
+
+    // Get domain length.
+    cur_rr_sec.name_len = strlen(cur_rr_sec.name);
+
+    // Get RR type.
+    offset += dm_occupied_len;
+    memcpy(&cur_rr_sec.rr_type, dns_pkt + offset, sizeof(uint16_t));
+    cur_rr_sec.rr_type = SWAP16(cur_rr_sec.rr_type);
+
+    // Get RR class.
+    offset += sizeof(uint16_t);
+    memcpy(&cur_rr_sec.rr_class, dns_pkt + offset, sizeof(uint16_t));
+    cur_rr_sec.rr_class = SWAP16(cur_rr_sec.rr_class);
+
+    // Get TTL.
+    offset += sizeof(uint16_t);
+    memcpy(&cur_rr_sec.ttl, dns_pkt + offset, sizeof(uint32_t));
+    cur_rr_sec.ttl = SWAP32(cur_rr_sec.ttl);
+        
+    // Get RDATA length.
+    offset += sizeof(uint32_t);
+    memcpy(&cur_rr_sec.rd_len, dns_pkt + offset, sizeof(uint16_t));
+    cur_rr_sec.rd_len = SWAP16(cur_rr_sec.rd_len);
+
+    // Get RDATA.
+    offset += sizeof(uint16_t);
+    cur_rr_sec.rdata = (char8_t*)malloc(sizeof(char8_t) * cur_rr_sec.rd_len);
+    memcpy(cur_rr_sec.rdata, dns_pkt + offset, cur_rr_sec.rd_len);
+
+    offset += cur_rr_sec.rd_len;
+}
+
+void parse_dns_pkt(char8_t* dns_pkt)
+{
+    PcapDNSHeader dns_header;
+    memcpy(&dns_header, dns_pkt, sizeof(PcapDNSHeader));
+
+    // Modify the byte order.
+    dns_header.trans_id = SWAP16(dns_header.trans_id);
+    dns_header.flags = SWAP16(dns_header.flags);
+    dns_header.qd_cnt = SWAP16(dns_header.qd_cnt);
+    dns_header.an_cnt = SWAP16(dns_header.an_cnt);
+    dns_header.ns_cnt = SWAP16(dns_header.ns_cnt);
+    dns_header.ar_cnt = SWAP16(dns_header.ar_cnt);
+
+    // Identify the flags.
+    // test, 暂时只识别方向和是否是nxdomain
+    bool is_query = false;
+    bool is_nxdomain = false;
+    if ((dns_header.flags & DNS_FLAG_QR_Q) == DNS_FLAG_QR_Q) {
+        is_query = true;
+    }
+    if (!is_query && (dns_header.flags & DNS_RCODE_NXDOMAIN) == DNS_RCODE_NXDOMAIN) {
+        is_nxdomain = true;
+    }
+
+    printf("---- ID: 0x%04x ---- Query: %d ---- Answer: %d ---- Authority: %d ---- Additional: %d ----\n", 
+            dns_header.trans_id, dns_header.qd_cnt, dns_header.an_cnt, dns_header.ns_cnt, dns_header.ar_cnt);
+
+    // Assign the Question, Answer RR, Authority RR, and Additional RR sections based on the DNS header info, respectively.
+    // Find the corresponding sections based on a cumulative offset.
+    uint16_t offset = sizeof(PcapDNSHeader);
+    // Assign the Question sections.
+    vector<PcapDNSQuestion> dns_question_list(dns_header.qd_cnt);
+    for (uint16_t i = 0; i < dns_header.qd_cnt; i++) {
+        PcapDNSQuestion cur_ques_sec;
+        get_question_section_info(dns_pkt, offset, cur_ques_sec);
+        dns_question_list[i] = cur_ques_sec;
+
+        printf("Query - %d: %s, %d, 0x%04x\n", i, cur_ques_sec.name, cur_ques_sec.qtype, cur_ques_sec.qclass);
+    }
+
+    // Assign the Answer RR sections.
+    vector<PcapDNSRR> dns_answer_list(dns_header.an_cnt);
+    for (uint16_t i = 0; i < dns_header.an_cnt; i++) {
+        PcapDNSRR cur_ans_sec;
+        get_rr_section_info(dns_pkt, offset, cur_ans_sec);
+        dns_answer_list[i] = cur_ans_sec;
+
+        printf("Answer - %d: %s, %d, 0x%04x, %d, %d\n", i, cur_ans_sec.name, cur_ans_sec.rr_type, cur_ans_sec.rr_class, cur_ans_sec.ttl, cur_ans_sec.rd_len);
+    }
+
+    // Assign the Authority RR sections.
+    vector<PcapDNSRR> dns_authority_list(dns_header.ns_cnt);
+    for (uint16_t i = 0; i < dns_header.ns_cnt; i++) {
+        PcapDNSRR cur_auth_sec;
+        get_rr_section_info(dns_pkt, offset, cur_auth_sec);
+        dns_authority_list[i] = cur_auth_sec;
+
+        printf("Authority - %d: %s\n", i, cur_auth_sec.name);
+    }
+
+    // Assign the Additional RR sections.
+    vector<PcapDNSRR> dns_additional_list(dns_header.ar_cnt);
+    for (uint16_t i = 0; i < dns_header.ar_cnt; i++) {
+        PcapDNSRR cur_add_sec;
+        get_rr_section_info(dns_pkt, offset, cur_add_sec);
+        dns_additional_list[i] = cur_add_sec;
+
+        printf("Additional - %d: %s\n", i, cur_add_sec.name);
+    }
+
+    // Free the allocated domain momery.
+    free_question_section(dns_question_list);
+    free_rr_section(dns_answer_list);
+    free_rr_section(dns_authority_list);
+    free_rr_section(dns_additional_list);
+}
--- app_layer_protos/dns.h
+++ app_layer_protos/dns.h
@@ -1,1 +1,153 @@
+/**
+ * The details of DNS protocol format can refer to RFC1035.
+ * https://www.rfc-editor.org/rfc/rfc1035.html
+ * Also can refer to IANA document.
+ * https://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml
+*/
 
+#ifndef _DNS_H_
+#define _DNS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "../base_type.h"
+
+using std::vector;
+using std::string;
+
+// DNS flags. More RCODEs need to be complemented.
+#define DNS_FLAG_QR_Q      0x8000
+#define DNS_FLAG_QR_R      0x0000
+#define DNS_FLAG_OP_SQUERY 0x0000
+#define DNS_FLAG_OP_IQUERY 0x0800
+#define DNS_FLAG_OP_STATUS 0x1000
+#define DNS_FLAG_OP_NOTIFY 0x2000
+#define DNS_FLAG_OP_UPDATE 0x2800
+#define DNS_FLAG_OP_DSO    0x3000
+#define DNS_FLAG_AA_N      0x0000
+#define DNS_FLAG_AA_Y      0x0400
+#define DNS_FLAG_TC_N      0x0000
+#define DNS_FLAG_TC_Y      0x0200
+#define DNS_FLAG_RD_N      0x0000
+#define DNS_FLAG_RD_Y      0x0100
+#define DNS_FLAG_RA_N      0x0000
+#define DNS_FLAG_RA_Y      0x0080
+#define DNS_FLAG_AD_N      0x0000
+#define DNS_FLAG_AD_Y      0x0020
+#define DNS_FLAG_CD_N      0x0000
+#define DNS_FLAG_CD_Y      0x0010
+#define DNS_RCODE_NOERR    0x0000
+#define DNS_RCODE_FMERR    0x0001
+#define DNS_RCODE_SERVFAIL 0x0002
+#define DNS_RCODE_NXDOMAIN 0x0003
+
+// DNS types. More types need to be complemented.
+#define DNS_TYPE_A         0x0001
+#define DNS_TYPE_NS        0x0002
+#define DNS_TYPE_MD        0x0003
+#define DNS_TYPE_MF        0x0004
+#define DNS_TYPE_CNAME     0x0005
+#define DNS_TYPE_SOA       0x0006
+#define DNS_TYPE_MB        0x0007
+#define DNS_TYPE_MG        0x0008
+#define DNS_TYPE_MR        0x0009
+#define DNS_TYPE_NULL      0x000a
+#define DNS_TYPE_WKS       0x000b
+#define DNS_TYPE_PTR       0x000c
+#define DNS_TYPE_HINFO     0x000d
+#define DNS_TYPE_MINFO     0x000e
+#define DNS_TYPE_MX        0x000f
+#define DNS_TYPE_TXT       0x0010
+#define DNS_TYPE_AAAA      0x001c
+#define DNS_TYPE_SRV       0x0021
+#define DNS_TYPE_DNAME     0x0027
+#define DNS_TYPE_NINFO     0x0038
+#define DNS_TYPE_HTTPS     0x0041
+#define DNS_TYPE_IXFR      0x00fb
+#define DNS_TYPE_AXFR      0x00fc
+#define DNS_TYPE_MAILB     0x00fd
+#define DNS_TYPE_MAILA     0X00fe
+#define DNS_TYPE_ANY       0x00ff
+
+// DNS classes. More classes need to be complemented.
+#define DNS_CLASS_IN       0x0001
+#define DNS_CLASS_CS       0x0002
+#define DNS_CLASS_CH       0x0003
+#define DNS_CLASS_HS       0x0004
+#define DNS_CLASS_NONE     0x00fe
+#define DNS_CLASS_ANY      0x00ff
+
+/**
+ * Message compression (zip mode) flag.
+ * In the zip mode 2-byte, the first 2 bits are specified as '11', 
+ *      and the remaining 14 bits are organized as the offset from the ID field.
+ *     +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+ *     | 1  1|                OFFSET                   |
+ *     +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+*/
+#define DNS_ZIP_FLAG       0xc0
+#define DNS_ZIP_MASK       0x3fff
+
+// DNS packet header. 12 bytes.
+typedef struct PcapDNSHeader {
+    uint16_t trans_id;
+    uint16_t flags;         // The highest 1-bit (QR) indicates whether the message is a query (0) or a reply (1).
+                            // The next 4-bit (OPCODE) represents the opcode.
+                            // The next 1-bit (AA) indicates whether the responding DNS server is authoritative.
+                            // The next 1-bit (TC) indicates whether the message is truncated.
+                            // The next 1-bit (RD) indicates whether the client desired a recursive query.
+                            // The next 1-bit (RA) indicates whether the responding DNS server supports recursive query.
+                            // The next 1-bit (Z) is reserved.
+                            // The next 1-bit (AD) indicates in a response packet that whether the response data has been verified by the responding DNS server.
+                            // The next 1-bit (CD) indicates in a query packet that whether the non-verified data is acceptable by the client.
+                            // The last 4-bit (RCODE) represents the respond code.
+    uint16_t qd_cnt;
+    uint16_t an_cnt;
+    uint16_t ns_cnt;
+    uint16_t ar_cnt;
+} PcapDNSHeader;
+
+/**
+ * DNS packet Question section. 
+ * A DNS packet may contain more than one Question section.
+*/
+typedef struct PcapDNSQuestion {
+    char8_t* name;
+    uint16_t name_len;
+    uint16_t qtype;
+    uint16_t qclass;
+} PcapDNSQuestion;
+
+/**
+ * The Answer, Authority, and Additional sections all share the same format:
+ * a variable number of RRs, where the number of records is specified in the
+ * corresponding count field in the DNS header.
+ * 
+ * Different RR types have different RDATA format.
+*/
+typedef struct PcapDNSRR {
+    char8_t* name;          // Notice, e.g., the '0xc00c', in the domain field, which is the 'message compression' scheme in DNS.
+                            // In message compression scheme, an entire domain is replaced with a pointer to a prior occurance of the same domain.
+                            // The message compression bytes occupies two bytes, and the first 2 bits are ones.
+                            // More details about the message compression scheme can refer to https://www.rfc-editor.org/rfc/rfc1035.html#section-4.1.4.
+    uint16_t name_len;
+    uint16_t rr_type;
+    uint16_t rr_class;
+    uint32_t ttl;
+    uint16_t rd_len;
+    char8_t* rdata;
+} PcapDNSRR;
+
+uint16_t get_domain(char8_t* dns_pkt, uint16_t offset, string& domain);
+void free_question_section(vector<PcapDNSQuestion>& dns_question_list);
+void free_rr_section(vector<PcapDNSRR>& dns_rr_list);
+void get_question_section_info(char8_t* dns_pkt, uint16_t& offset, PcapDNSQuestion& cur_ques_sec);
+void get_rr_section_info(char8_t* dns_pkt, uint16_t& offset, PcapDNSRR& cur_rr_sec);
+void parse_dns_pkt(char8_t* dns_pkt);
+
+#endif
